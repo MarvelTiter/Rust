7f07d50cb923e0d4227d6adc641ef5ca2cc7d904
marvel <outlinmt@outlook.com>
2023-12-10 10:19:58 +0800

    修改输入框为LineEdit组件，隐藏控制台窗口

--- slint-demo/src/main.rs
+++ slint-demo/src/main.rs
@@ -1,10 +1,10 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 slint::include_modules!();
-
-fn main() {
+fn main() -> Result<(), slint::PlatformError> {
     let app = App::new().unwrap();
     let weak_app = app.as_weak().unwrap();
-    app.on_create_qr_code(move||{
-        weak_app.set_plate("test".into());
+    app.on_create_qr_code(move|plate, pclass|{
+        println!("{} - {}", plate, pclass)
     });
-   let _= app.run();
+   app.run()
 }
